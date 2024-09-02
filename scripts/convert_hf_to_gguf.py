e35d8b82a5efac36719cc99ed91c5d0c202919aa
EeyoreLee <li.chunyu0412@gmail.com>
2024-09-02 13:43:02 +0800

    add requirements for python

--- scripts/convert_hf_to_gguf.py
+++ scripts/convert_hf_to_gguf.py
@@ -3,3 +3,16 @@
 @create_time: 2024/08/29 19:55:11
 @author: lichunyu
 '''
+import gguf
+from transformers import BertTokenizer
+
+
+class BertModel(object):
+
+    model_arch = gguf.MODEL_ARCH.BERT
+
+    def __init__(self) -> None:
+        pass
+
+    def write(self):
+        ...
